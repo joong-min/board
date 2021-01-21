bc4479cd769c9bd2216478a58b46290f61e3fd17
chacha86 <taejincha0619@gmail.com>
2021-01-21 19:38:49 +0900

    요구사항 7,8 완료

--- src/board/Main.java
+++ src/board/Main.java
@@ -5,11 +5,21 @@ import java.util.Scanner;
 
 public class Main {
 
+	static ArrayList<Article> articles = new ArrayList<>();
+	
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
-		ArrayList<Article> articles = new ArrayList<>();
-		int lastId = 1;
+		Article article1 = new Article(1, "제목1", "내용1");
+		Article article2 = new Article(2, "제목2", "내용2");
+		Article article3 = new Article(3, "제목3", "내용3");
+		
+		articles.add(article1);
+		articles.add(article2);
+		articles.add(article3);
+		
+		int lastId = 4;
 		
 		while (true) {
 			System.out.println("명령어 입력해주세요 :");
@@ -48,13 +58,7 @@ public class Main {
 			else if(str.equals("update")) {
 				System.out.println("수정할 게시물 번호 :");
 				int id = sc.nextInt();
-				int targetIdx = -1; 
-				
-				for(int i = 0; i < articles.size(); i++) {
-					if(articles.get(i).id == id) {
-						targetIdx = i;
-					}
-				}
+				int targetIdx = getIndexByArticleId(id);
 				
 				if(targetIdx != -1) {
 					System.out.println("제목 : ");
@@ -78,13 +82,7 @@ public class Main {
 				
 				System.out.println("삭제할 게시물 번호 :");
 				int id = sc.nextInt();
-				int targetIdx = -1; 
-				
-				for(int i = 0; i < articles.size(); i++) {
-					if(articles.get(i).id == id) {
-						targetIdx = i;
-					}
-				}
+				int targetIdx = getIndexByArticleId(id);
 				
 				if(targetIdx != -1) {
 					
@@ -94,10 +92,40 @@ public class Main {
 				} else {
 					System.out.println("없는 게시물입니다.");
 				}
+			} else if(str.equals("read")) {
+				System.out.println("상세보기 할 게시물 번호 :");
+				int id = sc.nextInt();
+				int targetIdx = getIndexByArticleId(id);
 				
+				if(targetIdx != -1) {
+					Article article = articles.get(targetIdx);
+					
+					System.out.println("====== " + article.id + "번 게시물 ======");
+					System.out.println("번호 : " + article.id);
+					System.out.println("제목 : " + article.title);
+					System.out.println("내용 : " + article.body);
+					System.out.println("========================");
+					
+				} else {
+					System.out.println("없는 게시물입니다.");
+				}
 			}
 		}
 
 	}
+	
+	public static int getIndexByArticleId(int id) {
+		int targetIdx = -1; 
+		
+		for(int i = 0; i < articles.size(); i++) {
+			if(articles.get(i).id == id) {
+				targetIdx = i;
+			}
+		}
+		
+		return targetIdx;
+	}
+	
+	
 
 }
